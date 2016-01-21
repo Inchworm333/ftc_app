852428fb69020a3dfa9782c5daadcae579f149e6
Inchworm333 <Inchworm333@gmail.com>
2016-01-21 16:54:24 -0500

    Update 1/21/16: added 3 new motors for slider and added servos for grabbing

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpSung.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpSung.java
@@ -2,23 +2,42 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class OpSung extends OpMode {
-    /*DC Motor*/
+    //DC Motors//
     DcMotor leftMotor1;
     DcMotor leftMotor2;
     DcMotor rightMotor1;
     DcMotor rightMotor2;
+    DcMotor sliderLeft;
+    DcMotor sliderRight;
+    DcMotor sliderCenter;
+
+    //Servos//
+    Servo grabberLeft;
+    Servo grabberRight;
+    Servo grabberCenter;
 
     @Override
     public void init() {
-        //defining motors
-        leftMotor1 = hardwareMap.dcMotor.get("motor_1");
-        leftMotor2 = hardwareMap.dcMotor.get("motor_2");
-        rightMotor1 = hardwareMap.dcMotor.get("motor_3");
-        rightMotor2 = hardwareMap.dcMotor.get("motor_4");
+        //driving motors//
+        leftMotor1 = hardwareMap.dcMotor.get("left_1");
+        leftMotor2 = hardwareMap.dcMotor.get("left_2");
+        rightMotor1 = hardwareMap.dcMotor.get("right_1");
+        rightMotor2 = hardwareMap.dcMotor.get("right_2");
 
-        //reverse the left motors
+        //slider motors//
+        sliderLeft = hardwareMap.dcMotor.get("slider_left");
+        sliderRight = hardwareMap.dcMotor.get("slider_right");
+        sliderCenter = hardwareMap.dcMotor.get("slider_center");
+
+        //slider servos//
+        grabberLeft = hardwareMap.servo.get("grabber_left");
+        grabberRight = hardwareMap.servo.get("grabber_right");
+        grabberCenter = hardwareMap.servo.get("grabber_center");
+
+        //reverse the left motors//
         leftMotor2.setDirection(DcMotor.Direction.REVERSE);
         rightMotor1.setDirection(DcMotor.Direction.REVERSE);
 
@@ -26,18 +45,23 @@ public class OpSung extends OpMode {
 
     @Override
     public void loop() {
-        //values from gamepad
-        double frontleft = -gamepad1.left_stick_y *0.7;
-        double frontright = -gamepad1.right_stick_y *0.7;
-        double backleft = frontleft * 0.7;
-        double backright = frontright * 0.7;
+        //values from gamepad1 driving//
+        double front_left = -gamepad1.left_stick_y *0.7;
+        double front_right = -gamepad1.right_stick_y *0.7;
+        double back_left = front_left * 0.7;
+        double back_right = front_right * 0.7;
+
+        //values from gamepad2 sliding//
+        double slider_left = gamepad2.left_stick_y;
+        double slider_right = gamepad2.right_stick_y;
+        double slider_center = a
 
 
-        //set motor power with gamepad
-        leftMotor1.setPower(frontleft);
-        rightMotor1.setPower(frontright);
+        //set motor power with gamepad//
+        leftMotor1.setPower(front_left);
+        rightMotor1.setPower(front_right);
 
-        rightMotor2.setPower(backright);
-        leftMotor2.setPower(backleft);
+        leftMotor2.setPower(back_left);
+        rightMotor2.setPower(back_right);
     }
 }
